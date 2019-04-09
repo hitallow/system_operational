02e2000e594c107784a3cda84b1e9d39b1f92a8b
Hitallo William <hitallo91@gmail.com>
2019-04-09 07:52:30 -0300

    leitores

--- leitores_e_escritores.c
+++ leitores_e_escritores.c
@@ -19,57 +19,65 @@ struct semaforos {
     int mutex; 
     int sem;
 };
-void writer(struct semaforos *sem){
+struct semaforos sem ; 
+void init(){
+    sem.sem  = sem_create(KEY,1); // sem
+    sem.mutex = sem_create(KEY,1);  //mutex
+}
+void writer(){
     do{
-        wait_d(sem->sem);
+        wait_d(sem.sem);
+            printf("\n\nEstou na função writter e sou o processo %d\n",getpid());
             printf("Estou escrevendo no arquivo\n");
-        signal_d(sem->sem);
+        signal_d(sem.sem);
         printf("Liberei o semaforo\n\n");
     } while (escritorI--);
 }
-void reader(struct semaforos *sem ){
-    printf("\t\t\nCheguei na função leitor\n");
+void reader(){
+    printf("\n\n\t\tCheguei na função leitor\n");
     do{
         // bloqueio o mutex
-        wait_d(sem->mutex);
+        wait_d(sem.mutex);
+            printf("\t\tEstou na função reader e sou o processo %d\n",getpid());
             readcount ++;
             // o primeiro processo, para o semaforo
             if(readcount == 1 )
             {
                 printf("\t\tSou o primeiro processo, vou bloquear o sem\n");
-                wait_d(sem->sem);
+                wait_d(sem.sem);
                 printf("\t\tbloqueei o processo!\n\n");
             }
             // libero o mutex
         printf("\t\tvou liberar o mutex\n\n");
-        signal_d(sem->mutex);
+        signal_d(sem.mutex);
+        printf("\t\tLiberei o mutex, agora vou ler em paz\n");
         printf("\t\tEstou lendo!\n");
         
 
         // bloqueio o mutex
-        wait_d(sem->mutex);
+        wait_d(sem.mutex);
             readcount -= 1 ;
             if(readcount == 0)
             {
-                signal_d(sem->sem);
+                signal_d(sem.sem);
             }
-        signal_d(sem->mutex);
+        signal_d(sem.mutex);
     }while(leitorI--);  
 }
 
+
 int main()
 {
-    struct semaforos sem ; 
     
-    sem.sem  = sem_create(1234,1); // sem
-    sem.mutex = sem_create(1234,1);  //mutex
-
-
-    // semaforos -> 1 > semaforos
-    // semafotos > 2 > mutex 
     
-    pthread_create(&escritor , NULL ,(void*) writer , &sem );
-    pthread_create(&leitor ,   NULL ,(void*) reader , &sem );
+    
+    init();
+    
+    printf("mutex . %d\n\n",sem.mutex);
+    printf("sem . %d\n\n",sem.sem);
+    
+    pthread_create(&escritor , NULL ,(void*) writer , NULL );
+    pthread_create(&leitor ,   NULL ,(void*) reader , NULL );
     pthread_join(leitor , NULL);
     pthread_join(escritor , NULL);
     printf("\nTerminei de ler\n");
